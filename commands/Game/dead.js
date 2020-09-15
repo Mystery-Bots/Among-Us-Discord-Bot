0224efc2ba65325546d26d59654775a368efbfa6
The-Mystery <the-mystery@outlook.com>
2020-09-15 17:43:16 +1200

    New: Dead and Revive command

---
+++ commands/Game/dead.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const mysql = require("mysql")
+
+module.exports.run = async (bot, message, args) => {
+    let connection = mysql.createConnection(bot.database)
+    guild = message.guild.id
+    user = message.mentions.users.first()
+    if (!user) user = message.author
+    connection.query(`SELECT * FROM \`${guild}\``, async function (_error,results) {
+        if (!results){
+            await connection.query(`CREATE TABLE \`${guild}\` (memberid VARCHAR(255))`)
+            await connection.query(`INSERT INTO \`${guild}\` (memberid) VALUES ('${user.id}')`)
+        }
+        else{
+            connection.query(`INSERT INTO \`${guild}\` (memberid) VALUES ('${user.id}')`)
+        }
+        message.channel.send(`${user.tag} set as dead for round. When round is over use \`${bot.config.prefix}end\` to unmute all players.\nIf you made a mistake in listing someone as dead use \`${bot.config.prefix}revive\``)
+    })
+}
+
+module.exports.info = {
+    name: "dead",
+    description: "Set yourself or someone else to dead so they don't get unmuted when running the unmute command",
+    category: "Game",
+    aliases: ["d"],
+    GuildOnly: true
+}
---
+++ commands/Game/revive.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+const mysql = require("mysql")
+
+module.exports.run = async (bot, message, args) => {
+    let connection = mysql.createConnection(bot.database)
+    guild = message.guild.id
+    user = message.mentions.users.first()
+    if (!user) user = message.author
+    connection.query(`SELECT * FROM \`${guild}\``, async function (_error,results) {
+        if (!results){
+            message.channel.send(`${user.tag} is not listed as dead`)
+        }
+        else{
+            connection.query(`DELETE FROM \`${guild}\` WHERE memberid = '${user.id}'`)
+        }
+        message.channel.send(`${user.tag} Revived. To list people as dead use \`${bot.config.prefix}dead\`.\nTo unmute revived players use \`${bot.config.prefix}unmute\``)
+    })
+}
+
+module.exports.info = {
+    name: "revive",
+    description: "Revive yourself or someone else if you set them as dead on accident.",
+    category: "Game",
+    aliases: ["r"],
+    GuildOnly: true
+}
