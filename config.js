3233f83e4d734a25d6752c2a539969ddb3e6a3d8
The-Mystery <the-mystery@outlook.com>
2020-09-15 17:40:14 +1200

    New: Added database and other features

--- config.js
+++ config.js
@@ -1,4 +1,25 @@
 module.exports = {
-    token: "Your Token Here",
-    prefix: "au"
+    database: {
+        user: 'Username',
+        host: 'IP',
+        database: 'Database',
+        password: 'Password',
+        port: 3306,
+        bigNumberStrings: true,
+        supportBigNumbers: true,
+        multipleStatements: true,
+        typeCast: function castField( field, useDefaultTypeCasting ) { // Bool Support
+            if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+                var bytes = field.buffer();
+                return( bytes[ 0 ] === 1 );
+            }
+            return( useDefaultTypeCasting() );
+        }
+    },
+    discord:{
+        token: "Your Token Here",
+        prefix: "-",
+        devs: ["Dev ID"],
+        ignore: ["Server ID"]
+    }
 }
