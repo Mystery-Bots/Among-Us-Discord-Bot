e84261c6335af2f6b64f8ad2d99cdf8c2ed0e898
The-Mystery <the-mystery@outlook.com>
2020-09-15 17:42:29 +1200

    New: Added database support

--- commands/Game/unmute.js
+++ commands/Game/unmute.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const mysql = require("mysql")
 
 module.exports.run = async (bot, message, args) => {
     channel = message.member.voice.channel
@@ -11,10 +12,31 @@ module.exports.run = async (bot, message, args) => {
         return message.reply("Sorry but you are not connected to a voice chat for me to manage.")
     }
 
-    for ([memberID, member] of channel.members){
-        member.voice.setMute(false, "Among Us Game Chat Control")
-    }
-    message.channel.send("Users unmuted for round. To remute the voice chat please use" + `\`${bot.config.prefix}mute\`.`)
+    let connection = mysql.createConnection(bot.database)
+    guild = message.guild.id
+
+    connection.query(`SELECT * FROM \`${guild}\``, async function (_error,results) {
+        if (!results){
+            for ([memberID, member] of channel.members){
+                
+                member.voice.setMute(false, "Among Us Game Chat Control")
+            }
+        }
+        else{
+            deadUsers = []
+            for (deadUser of results){
+                deadUsers.push(deadUser.memberid)
+            }
+            for ([memberID, member] of channel.members){
+                if (deadUsers.includes(memberID)){}
+                else{
+                    member.voice.setMute(false, "Among Us Game Chat Control")
+                }
+            }
+        }
+    })
+    
+    message.channel.send("Users unmuted for round. To re-mute the voice chat please use" + `\`${bot.config.prefix}mute\`.`)
 
 }
 
