77f6129429a46f1d3870f72a6a8a38ebe70f2fea
The-Mystery <the-mystery@outlook.com>
2020-09-17 10:30:37 +1200

    Added: Bitfield and also chack for invite perms

--- commands/Developer/servers.js
+++ commands/Developer/servers.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     if (!bot.config.devs.includes(message.author.id)) return console.log(`${message.author.tag} (ID: ${message.author.id}) tried to use "servers"`)
+    message.channel.startTyping()
     embed = new Discord.MessageEmbed()
         .setTitle("Server List")
         for ([guildID, guild] of bot.guilds.cache){
@@ -9,8 +10,12 @@ module.exports.run = async (bot, message, args) => {
                 
             }
             else{
-                invites = await guild.fetchInvites()
-                if (!invites){
+                if (guild.me.hasPermission("MANAGE_GUILD")){
+                    invites = await guild.fetchInvites()
+                }else{
+                    invites = undefined
+                }
+                if (!invites){  
                     invite = "No Perms"
                 }else if (invites.size < 1){
                     invite = "No Invites"
@@ -18,9 +23,10 @@ module.exports.run = async (bot, message, args) => {
                 else{
                     invite = invites.first()
                 }
-                embed.addField(`${guild.name} (ID: ${guild.id})`, `Member Count: ${guild.memberCount}\nInvite: ${invite}`,true)
+                embed.addField(`${guild.name} (ID: ${guild.id})`, `Member Count: ${guild.memberCount}\nPermissions: ${guild.me.permissions.bitfield}\nInvite: ${invite}`,true)
             }
         }
+        message.channel.stopTyping(true)
         message.channel.send(embed)
 }
 
