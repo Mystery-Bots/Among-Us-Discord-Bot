1cc8cd340d70e897015f2d7216d5b58d46d41156
The-Mystery <the-mystery@outlook.com>
2020-09-17 21:02:16 +1200

    removed ready cache

--- discordEvents/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-let config = require("../config")
-module.exports.Run = function(bot){
-        bot.user.setPresence({status: 'online' })
-        require('../services/botStatus').Run(bot)
-}
