a03bc736fc1ad9651fc746418950421a67931126
The-Mystery <the-mystery@outlook.com>
2020-09-17 20:19:22 +1200

    Change: Guilds -> Servers

--- services/botStatus.js
+++ services/botStatus.js
@@ -4,7 +4,7 @@ module.exports.Run = async function(bot){
     console.log("Bot Ready")
     setInterval(async function(){
         let statuses = [
-            `${bot.config.prefix}help | ${bot.guilds.cache.size} Guilds!`,
+            `${bot.config.prefix}help | ${bot.guilds.cache.size} Servers!`,
             `${bot.config.prefix}help | TheMystery.me/Among-Us`,
         ]
         currentStatus = currentStatus + 1 < statuses.length ? currentStatus + 1 : 0
