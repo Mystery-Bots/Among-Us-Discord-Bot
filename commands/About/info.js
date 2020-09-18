26549964f55c14e92ad6ed8017b3283832c20fec
The-Mystery <the-mystery@outlook.com>
2020-09-18 21:32:44 +1200

    Added: Random crewmate color on each run

--- commands/About/info.js
+++ commands/About/info.js
@@ -1,6 +1,21 @@
 const Discord = require('discord.js')
 const ms = require('ms')
 
+crewColor = [
+    "https://themystery.s-ul.eu/bot/l4PQmeQ3",
+    "https://themystery.s-ul.eu/bot/kj0elCnG",
+    "https://themystery.s-ul.eu/bot/BkgP4wyw",
+    "https://themystery.s-ul.eu/bot/zuFhrjTA",
+    "https://themystery.s-ul.eu/bot/Fi1yfLwo",
+    "https://themystery.s-ul.eu/bot/EplSSNXt",
+    "https://themystery.s-ul.eu/bot/a3nnrBmj",
+    "https://themystery.s-ul.eu/bot/mw4KhkOT",
+    "https://themystery.s-ul.eu/bot/0IssEuGF",
+    "https://themystery.s-ul.eu/bot/UYr66rph",
+    "https://themystery.s-ul.eu/bot/v6QOWIvQ",
+    "https://themystery.s-ul.eu/bot/5MT8kSEi"
+]
+
 module.exports.run = async (bot, message, args) => {
     userCount = 0
     for ([guildID, guild] of bot.guilds.cache){
@@ -10,10 +25,12 @@ module.exports.run = async (bot, message, args) => {
             userCount += guild.memberCount
         }
     }
+    color = Math.floor(Math.random() * crewColor.length) //Number 0 to 11
     embed = new Discord.MessageEmbed()
         .setTitle("Among Us Bot Info")
         .setURL("https://themystery.me/Among-Us")
         .setFooter("Created by TheMystery#7755")
+        .setThumbnail(crewColor[color])
         .setDescription("The only Among Us Discord bot you need for your friends group/server.")
         .addField("Ping", `${bot.ws.ping.toFixed(1)}ms`,true)
         .addField("\u200B", "\u200B",true)
