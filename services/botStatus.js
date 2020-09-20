0c212a67e694c41a6318ea52054081f8bdd25eec
The-Mystery <the-mystery@outlook.com>
2020-09-20 20:10:28 +1200

    New: Website Link

--- services/botStatus.js
+++ services/botStatus.js
@@ -5,7 +5,7 @@ module.exports.Run = async function(bot){
     setInterval(async function(){
         let statuses = [
             `${bot.config.prefix}help | ${bot.guilds.cache.size} Servers!`,
-            `${bot.config.prefix}help | TheMystery.me/Among-Us`,
+            `${bot.config.prefix}help | aub.TheMystery.me`,
         ]
         currentStatus = currentStatus + 1 < statuses.length ? currentStatus + 1 : 0
         let status = statuses[currentStatus]
