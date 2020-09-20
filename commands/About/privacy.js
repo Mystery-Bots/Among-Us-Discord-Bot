7c10690fe176b79b5590aeb5d41b6fa0f18d8ac2
The-Mystery <the-mystery@outlook.com>
2020-09-20 20:09:53 +1200

    Created: Privacy link and Support server link

---
+++ commands/About/privacy.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send("Here is an link to my privacy policy.\n<:Invite:756026277311283292>https://aub.themystery.me/privacy<:Invite:756026277311283292>")
+
+}
+
+module.exports.info = {
+    name: "privacy",
+    description: "Get a link to the privacy policy for the bot.",
+    category: "About",
+}
---
+++ commands/Support/support.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send("Here is an invite for you to join the support server.\n<:Invite:756026277311283292>https://discord.gg/AD2a24y<:Invite:756026277311283292>")
+
+}
+
+module.exports.info = {
+    name: "support",
+    description: "Get bot invite to the support server",
+    category: "Support",
+}
