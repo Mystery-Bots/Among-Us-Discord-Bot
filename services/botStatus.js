0d2a1c408d2069e0621930623708a9b2c636d60f
The-Mystery <the-mystery@outlook.com>
2020-09-29 17:06:43 +1300

    Moved: Servercount to the servercount service

--- services/botStatus.js
+++ services/botStatus.js
@@ -1,32 +1,7 @@
-const { MongoClient } = require("mongodb");
 const mariadb  = require("mariadb")
 botStatus = require("../status.json").status
-const uri = "mongodb+srv://among-us-bot:BW3Lb86EifZOiu3U@cluster0.daswr.mongodb.net/bot?retryWrites=true&w=majority";
+
 currentStatus = 0;
-
-async function update(serverCount) {
-	const client = new MongoClient(uri, { useUnifiedTopology: true });
-	try {
-		await client.connect();
-
-		const database = client.db("bot");
-		const collection = database.collection("info");
-
-		// create a filter for a movie to update
-		const filter = { _id: "5f6c5183784bc0b5904a1b9d" };
-
-		// create a document that sets the plot of the movie
-		const updateDoc = {
-			$set: {
-				count: `${serverCount}`
-			},
-		};
-
-		const result = await collection.updateOne(filter, updateDoc);
-	} finally {
-		await client.close();
-	}
-}
 
 async function fetchStatus(bot){
 	let connection = await mariadb.createConnection(bot.database)
@@ -46,9 +21,6 @@ module.exports.Run = async function (bot) {
 			`${bot.config.prefix}help | aub.TheMystery.me`,
 		];
 		currentStatus = currentStatus + 1 < statuses.length ? currentStatus + 1 : 0;
-		if (currentStatus == 0) {
-			update(bot.guilds.size);
-		}
 		let status = statuses[currentStatus];
 		bot.editStatus(await fetchStatus(bot),{name: status, type: 0});
 	}, 60 * 1000);
--- services/serverCount.js
+++ services/serverCount.js
@@ -1,17 +1,50 @@
 const centra = require('centra')
+const { MongoClient } = require("mongodb");
 
 const DB = 'https://discord.bots.gg/api/v1/bots/754922494376542219/stats'
 const DBToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOnRydWUsImlkIjoiMjk1Nzk4MjUxMDIwNDg0NjA4IiwiaWF0IjoxNjAxMjgwMDQ1fQ.WZhkfbuu8AzyGUbYjsPMuUIPNSZayLRZUtPTwE5E3WY'
+const uri = "mongodb+srv://among-us-bot:BW3Lb86EifZOiu3U@cluster0.daswr.mongodb.net/bot?retryWrites=true&w=majority";
+
+async function update(serverCount) {
+	const client = new MongoClient(uri, { useUnifiedTopology: true });
+	try {
+		await client.connect();
+
+		const database = client.db("bot");
+		const collection = database.collection("info");
+
+		// create a filter for a movie to update
+		const filter = { _id: "5f6c5183784bc0b5904a1b9d" };
+
+		// create a document that sets the plot of the movie
+		const updateDoc = {
+			$set: {
+				count: serverCount
+			},
+		};
+
+        const result = await collection.updateOne(filter, updateDoc);
+        console.log(`Sent data: {count:${serverCount}} to the Website`)
+	} finally {
+		await client.close();
+	}
+}
+
+async function DBUpdate(bot){
+    result = await centra(DB,'POST')
+        .header("Authorization",DBToken)
+        .body({
+            "shardCount":bot.shards.size,
+            "guildCount":bot.guilds.size
+        }).send().then(res => {
+            console.log(`Sent data: ${res.body.toString()} to Discord Bots`)
+        })
+}
+
 
 module.exports.Run = async function (bot) {
     setInterval(async function() {
-        centra(DB,'POST')
-            .header("Authorization",DBToken)
-            .body({
-                "shardCount":bot.shards.size,
-                "guildCount":bot.guilds.size
-            }).send().then(res => {
-                console.log(`Sent data: ${res.body.toString()} to Discord Bots`)
-            })
+        await update(bot.guilds.size);
+        await DBUpdate(bot)
     },120 * 1000)
 };
