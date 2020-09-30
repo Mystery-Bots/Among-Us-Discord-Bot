b88a2fe38a9ee5365a4db71af6e7e66c1c647cba
The-Mystery <the-mystery@outlook.com>
2020-09-30 22:26:23 +1300

    Fixed: Allow bot to receive message events again

--- discord.js
+++ discord.js
@@ -6,7 +6,7 @@ const ms = require("ms");
 const moment = require("moment");
 
 const bot = new Discord.Client(config.discord.token, {
-	intents: 3,
+	intents: 515,
 	maxShards:2,
 	guildSubscriptions:false,
 	largeThreshold: 50,
