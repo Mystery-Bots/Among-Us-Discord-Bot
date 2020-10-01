e213f55ee8578e782ade1930ab0ad7532af6638c
The-Mystery <the-mystery@outlook.com>
2020-10-01 20:35:47 +1300

    Changed guild.id to guildID

--- commands/Game/dead.js
+++ commands/Game/dead.js
@@ -2,9 +2,7 @@ const mariadb  = require("mariadb")
 
 module.exports.run = async (bot, message, args) => {
     guild = message.channel.guild
-    if (!guild.id){
-        return message.channel.createMessage("There was an error. Please make sure you are not running this command in a DM or Group DM")
-    }
+    guildID = message.guildID
     userMention = message.mentions[0]
     if (!userMention) userMention = message.author
     member = await guild.members.find(user => user.id == userMention.id)
@@ -17,8 +15,8 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.createMessage("Sorry but you or the mentioned user are not connected to a voice chat for me to manage.")
     }
     let connection = await mariadb.createConnection(bot.database)
-    connection.query(`SELECT * FROM \`${guild.id}\``).then( async () => {
-        connection.query(`SELECT * FROM \`${guild.id}\` WHERE memberid = '${member.id}'`).then( async (rows) => {
+    connection.query(`SELECT * FROM \`${guildID}\``).then( async () => {
+        connection.query(`SELECT * FROM \`${guildID}\` WHERE memberid = '${member.id}'`).then( async (rows) => {
             if (!rows[0]){
                 let failed = false
                 try {
@@ -30,7 +28,7 @@ module.exports.run = async (bot, message, args) => {
                     return message.channel.createMessage("Sorry but I need permissions to Mute Members")
                 }
                 if (!failed){
-                    await connection.query(`INSERT INTO \`${guild.id}\` (memberid) VALUES ('${member.id}')`)
+                    await connection.query(`INSERT INTO \`${guildID}\` (memberid) VALUES ('${member.id}')`)
                     await connection.destroy();
                     message.channel.createMessage(`${member.user.username} set as dead for round. When round is over use \`${bot.config.prefix}end\` to unmute all players.\nIf you made a mistake in listing someone as dead use \`${bot.config.prefix}revive\`.`)
                 }
@@ -49,13 +47,16 @@ module.exports.run = async (bot, message, args) => {
                 return message.channel.createMessage("Sorry but I need permissions to Mute Members")
             }
             if (!failed){
-                await connection.query(`INSERT INTO \`${guild.id}\` (memberid) VALUES ('${member.id}')`)
+                await connection.query(`INSERT INTO \`${guildID}\` (memberid) VALUES ('${member.id}')`)
                 await connection.destroy();
                 message.channel.createMessage(`${member.user.username} set as dead for round. When round is over use \`${bot.config.prefix}end\` to unmute all players.\nIf you made a mistake in listing someone as dead use \`${bot.config.prefix}revive\`.`)
             }
         })
     }).catch( async (error) => {
         let failed = false
+        if (!guildID){
+            return message.channel.createMessage("There was an error. Please make sure you are not running this command in a DM or Group DM")
+        }
         try {
             await member.edit({mute:true}, "Among Us Game Chat Control")
         }
@@ -65,8 +66,8 @@ module.exports.run = async (bot, message, args) => {
             return message.channel.createMessage("Sorry but I need permissions to Mute Members")
         }
         if (!failed){
-            await connection.query(`CREATE TABLE \`${guild.id}\` (memberid VARCHAR(255))`)
-            await connection.query(`INSERT INTO \`${guild.id}\` (memberid) VALUES ('${member.id}')`)
+            await connection.query(`CREATE TABLE \`${guildID}\` (memberid VARCHAR(255))`)
+            await connection.query(`INSERT INTO \`${guildID}\` (memberid) VALUES ('${member.id}')`)
             await connection.destroy();
             message.channel.createMessage(`${member.user.username} set as dead for round. When round is over use \`${bot.config.prefix}end\` to unmute all players.\nIf you made a mistake in listing someone as dead use \`${bot.config.prefix}revive\`.`)
         }
