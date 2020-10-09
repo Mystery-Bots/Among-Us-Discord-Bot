f81f39bfcdb2357386d54b71d95cc7c11547940c
The-Mystery <the-mystery@outlook.com>
2020-10-09 15:53:42 +1300

    Added: Discord Bots List

--- services/serverCount.js
+++ services/serverCount.js
@@ -3,6 +3,8 @@ const { MongoClient } = require("mongodb");
 
 const DB = 'https://discord.bots.gg/api/v1/bots/754922494376542219/stats'
 const DBToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOnRydWUsImlkIjoiMjk1Nzk4MjUxMDIwNDg0NjA4IiwiaWF0IjoxNjAxMjgwMDQ1fQ.WZhkfbuu8AzyGUbYjsPMuUIPNSZayLRZUtPTwE5E3WY'
+const DBL = "https://discordbotlist.com/api/v1/bots/754922494376542219/stats"
+const DBLToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoxLCJpZCI6Ijc1NDkyMjQ5NDM3NjU0MjIxOSIsImlhdCI6MTYwMjIxMTI4OX0.JSiSSfGrwtT2UPYEsy_D2FQNNxtBhQwljMFLAWOwVkE"
 const uri = "mongodb+srv://among-us-bot:BW3Lb86EifZOiu3U@cluster0.daswr.mongodb.net/bot?retryWrites=true&w=majority";
 
 async function update(serverCount) {
@@ -41,10 +43,22 @@ async function DBUpdate(bot){
         })
 }
 
+async function DBLUpdate(bot){
+    result = await centra(DBL,'POST')
+        .header("Authorization",DBLToken)
+        .body({
+            "guilds":bot.guilds.size
+        }).send().then(res => {
+            console.log(`Sent data: ${res.body.toString()} to Discord Bots List`)
+        })
+}
+
+
 
 module.exports.Run = async function (bot) {
     setInterval(async function() {
         await update(bot.guilds.size);
         await DBUpdate(bot)
+        await DBLUpdate(bot)
     },120 * 1000)
 };
