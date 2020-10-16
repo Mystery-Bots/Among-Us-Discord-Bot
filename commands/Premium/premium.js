70d91aa1d369664ca3bcfef921551f5e7d2deb70
The-Mystery <the-mystery@outlook.com>
2020-10-16 16:10:38 +1300

    Moved and Removed: Moved to premium folder Removed premium giveaway message

--- commands/Premium/premium.js
+++ commands/Premium/premium.js
@@ -5,18 +5,14 @@ module.exports.run = async (bot, message, args) => {
         thumbnail: {
             url: "https://themystery.s-ul.eu/bot/Ih7eUxcH"
         },
-        description: "If you would like to help to keep the bot running.\nFeel free to buy premium [here](https://www.patreon.com/TheMystery)",
+        description: "If you would like to help to keep the bot running.\nFeel free to buy premium [here](https://aub.mysterybots.com/premium)",
         fields: [
             {
                 name:"Perks",
                 value:"-Colored Server Info Embed\n-Crewmate in your Server Info Embed\n-Role in [Support Server](https://discord.gg/AD2a24y)\n-Premium Support\n-Early previews of updates"
-            },
-            {
-                name:"Giveaway",
-                value:"I am giving away 3, one months of premium in my [Support Server](https://discord.gg/AD2a24y).\n**Join now for a chance to win.**"
             }
         ],
-        url: "https://www.patreon.com/TheMystery",
+        url: "https://aub.mysterybots.com/premium",
         color: 0x00ff00
     }}
     message.channel.createMessage(embedObject).catch((error) => {
@@ -29,6 +25,6 @@ module.exports.run = async (bot, message, args) => {
 module.exports.info = {
     name: "premium",
     description: "Get a link to get premium",
-    category: "Support",
+    category: "Premium",
     aliases:["donate"]
 }
