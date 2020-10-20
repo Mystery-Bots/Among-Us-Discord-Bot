e3d812e5e76c7573c36f9236ec0336466cc705a9
The-Mystery <the-mystery@outlook.com>
2020-10-21 12:49:45 +1300

    Fixed: Actually show average and not total

--- commands/About/shards.js
+++ commands/About/shards.js
@@ -28,7 +28,7 @@ module.exports.run = async (bot, message, args) => {
             embedObject.embed.fields.push({name:`Shard ${shardID}`, value: `Status: ${statuses[shard.status]}\nPing: ${isFinite(shard.latency) ? `${shard.latency} ms` : "Offline"}`, inline:true})
         }
     }
-    embedObject.embed.description = `Status of all bot shards\nAverage Ping: ${(pings.reduce((a, b) => a + b))} ms`,
+    embedObject.embed.description = `Status of all bot shards\nAverage Ping: ${pings.reduce((a, b) => a + b)/pings.length} ms`,
     message.channel.createMessage(embedObject).catch((error) => {
 		if (error.message == "Missing Permissions"){
 			message.channel.createMessage("I need `Embed Links` permissions to be able to send this message.")
