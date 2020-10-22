12cdf215720be5a03dc5f8869068fad9d7ece233
The-Mystery <the-mystery@outlook.com>
2020-10-23 08:49:55 +1300

    Added: Compress

--- discord.js
+++ discord.js
@@ -8,6 +8,7 @@ const moment = require("moment");
 const bot = new Discord.Client(config.discord.token, {
 	//intents: 4739,
 	intents:4737,
+	compress: true,
 	maxShards:"auto",
 	guildSubscriptions:false,
 	messageLimit:10,
