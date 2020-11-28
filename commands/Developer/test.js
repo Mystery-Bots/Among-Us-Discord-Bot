11e4dea85c4983ba6d902c423a2249505ffc5191
The-Mystery <the-mystery@outlook.com>
2020-11-28 13:52:27 +1300

    Removed: test command

--- commands/Developer/test.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    console.log(bot.users)
-}
-
-module.exports.info = {
-    name: "test",
-    description: "Testing command",
-    category: "Developer",
-}
