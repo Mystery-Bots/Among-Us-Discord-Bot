c24f663c02d9d2af9c01620a69be298cda3a1957
The-Mystery <the-mystery@outlook.com>
2020-11-28 13:39:09 +1300

    Fixed: was in ready statement

--- services/memory.js
+++ services/memory.js
@@ -4,7 +4,6 @@ module.exports.Run = async function (bot) {
     console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     setInterval(async function() {
         console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
-        console.log(bot.users.length)
         delete bot.users
         bot.users = new Discord.Collection(Discord.User)
     }, 60 * 1000)
