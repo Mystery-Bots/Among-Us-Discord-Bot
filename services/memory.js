13613984140cd2f1306d90f268de2b244ddefe1d
The-Mystery <the-mystery@outlook.com>
2020-12-11 09:53:29 +1300

    Reverted: Reverted update for cache guilds due to breaking commands

--- services/memory.js
+++ services/memory.js
@@ -4,9 +4,7 @@ module.exports.Run = async function (bot) {
     console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     setInterval(async function() {
         delete bot.users
-        delete bot.guilds
         bot.users = new Discord.Collection(Discord.User)
-        bot.guilds = new Discord.Collection(Discord.Guild)
         console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     }, 60 * 1000)
 };
