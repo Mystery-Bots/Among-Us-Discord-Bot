a8a771bbb5c6fa8f0057ffc1d4d9af8f4ef9001f
TheMystery <the-mystery@outlook.com>
2020-12-25 12:35:30 +1300

    Added: deleting of server cache

--- services/memory.js
+++ services/memory.js
@@ -5,6 +5,8 @@ module.exports.Run = async function (bot) {
     setInterval(async function() {
         delete bot.users
         bot.users = new Discord.Collection(Discord.User)
+        delete bot.guilds
+        bot.guilds = new Discord.Collection(Discord.Guild)
         console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     }, 60 * 1000)
 };
