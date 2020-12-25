e9bad35751b68af42861d72c9b16e7aa654eb146
TheMystery <the-mystery@outlook.com>
2020-12-25 21:26:47 +1300

    Removed: unneeded code

--- services/memory.js
+++ services/memory.js
@@ -1,5 +1,3 @@
-Discord = require('eris')
-
 module.exports.Run = async function (bot) {
     console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     setInterval(async function() {
