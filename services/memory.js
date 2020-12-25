234d202de00b6476cbfbd3c6fc65fdc458644697
TheMystery <the-mystery@outlook.com>
2020-12-25 20:10:28 +1300

    Removed: Stuff from memory

--- services/memory.js
+++ services/memory.js
@@ -3,10 +3,6 @@ Discord = require('eris')
 module.exports.Run = async function (bot) {
     console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     setInterval(async function() {
-        delete bot.users
-        bot.users = new Discord.Collection(Discord.User)
-        delete bot.guilds
-        bot.guilds = new Discord.Collection(Discord.Guild)
         console.log(`${Math.round((process.memoryUsage().heapUsed / 1024 / 1024)*100)/100} MB`)
     }, 60 * 1000)
 };
